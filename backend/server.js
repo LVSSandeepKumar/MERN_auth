2d12e72e9c3f041e7ff2e97307f928ec2944354e
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-08 17:10:53 +0530

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { ENV_VARS } from "./utils/envVars.js";
+import { connectDB } from "./utils/connectDB.js";
 
+//Setup the express app
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
+//Listen to the server at defined port
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
+    connectDB();
 })
--- backend/utils/envVars.js
+++ backend/utils/envVars.js
@@ -3,5 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-    PORT: process.env.PORT || 8000
+    PORT: process.env.PORT || 8000,
+    MONGODB_URI: process.env.MONGODB_URI,
 }
