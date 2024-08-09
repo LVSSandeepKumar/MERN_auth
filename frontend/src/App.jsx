d8a9000ac19504bd5b031110d01accafcbfa2c44
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-09 14:11:32 +0530

    Frontend Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
 function App() {
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-500 to-emerald-900
-    flex items-center justify-center overflow-hidden relative">
-      <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0}/>
-      <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5}/>
-      <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2}/>
-    </div>
+    <h1 className="text-red-500 text-center"> Hello </h1>  
   )
-}j
+}
 
 export default App
