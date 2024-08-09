0f21cf03f1a21298b586a135a6ee5f4954916407
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-09 13:16:08 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-500 to-emerald-900
+    flex items-center justify-center overflow-hidden relative">
+
+    </div>
+  )
+}
+
+export default App
